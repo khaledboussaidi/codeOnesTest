bd254f845af41125b2e7829bf4a4e9e8be5a1504
khaledboussaidi <khaled.boussaidi@etudiant-enit.utm.tn>
2021-09-16 11:47:13 +0100

    first commit

---
+++ ponctuiation-service/src/main/java/com/ponctuiation/ponctuiationservice/PonctuiationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ponctuiation.ponctuiationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PonctuiationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PonctuiationServiceApplication.class, args);
+	}
+
+}
